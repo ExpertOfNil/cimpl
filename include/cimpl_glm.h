7574e7ed301a11d018c619a0a861aa6bec9dfe05
Matt McKenna <matthew.w.mckenna@spiritaero.com>
2025-10-31 15:43:52 -0500

    Add basic GL math functions
    - Changed a couple function names for clarity
    - Need to establish a good naming pattern
    - Made some organization adjustments
    - Choose to copy instead of reference, for now.  Maybe choose to use
      const pointers to indicate read-only

--- include/cimpl_glm.h
+++ include/cimpl_glm.h
@@ -1,12 +1,30 @@
 #ifndef CIMPL_GLM_H
 #define CIMPL_GLM_H
 
-#include "cimpl_core.h"
 #include <fcntl.h>
+#include <math.h>
 #include <unistd.h>
+
+#include "cimpl_core.h"
 
 #define POSE_PRINT_FORMAT \
     "[%010d] %9.3f, %9.3f, %9.3f,%9.3f, %9.3f, %9.3f, %9.3f\n"
+#define QUAT_IDENTITY {0.0f, 0.0f, 0.0f, 1.0f};
+// clang-format off
+#define MAT3_IDENTITY { \
+    1.0f, 0.0f, 0.0f, \
+    0.0f, 1.0f, 0.0f, \
+    0.0f, 0.0f, 1.0f, \
+}
+// clang-format on
+// clang-format off
+#define MAT4_IDENTITY { \
+    1.0f, 0.0f, 0.0f, 0.0f, \
+    0.0f, 1.0f, 0.0f, 0.0f, \
+    0.0f, 0.0f, 1.0f, 0.0f, \
+    0.0f, 0.0f, 0.0f, 1.0f, \
+}
+// clang-format on
 
 typedef enum {
     AXIS_X = 0,
@@ -29,10 +47,6 @@ struct Vec3Node {
 };
 
 DEFINE_DYNAMIC_ARRAY(Vec3Node, Vec3Tree)
-void Vec3Tree_print(Vec3Tree*);
-i32 Vec3Tree_partition(Vec3Tree*, Axis, i32, i32);
-void Vec3Tree_quicksort(Vec3Tree*, Axis, i32, i32);
-CimplReturn Vec3Tree_sort(Vec3Tree*, Axis, const Vec3Array*);
 
 typedef struct StlTriangle {
     Vec3 normal;
@@ -62,28 +76,197 @@ typedef struct Mat4 {
     f32 ti, tj, tk, tw;
 } Mat4;
 
-Mat3 Mat4_rotation(Mat4*);
-Vec3 Mat4_translation(Mat4*);
+/*** FUNCTION DECLARATIONS ***/
+
+f32 Vec3_length(Vec3 v);
+Vec3 Vec3_normalize(Vec3 v);
+Vec3 Vec3_cross(Vec3 a, Vec3 b);
+Vec3 Vec3_translation_from_mat4(Mat4 m);
+
+void Vec3Tree_print(Vec3Tree*);
+i32 Vec3Tree_partition(Vec3Tree*, Axis, i32, i32);
+void Vec3Tree_quicksort(Vec3Tree*, Axis, i32, i32);
+CimplReturn Vec3Tree_sort(Vec3Tree*, Axis, const Vec3Array*);
+
+f32 Quat_length(Quat q);
+Quat Quat_normalize(Quat q);
+
+Mat3 Mat3_from_quat(Quat q);
+Mat3 Mat3_rotation_from_mat4(Mat4 m);
+
+Mat4 Mat4_with_rotation(Mat4 dst, Mat3 src);
+void Mat4_print_with_id(Mat4 m, const char* id);
+Mat4 Mat4_from_translation_quat(Vec3 t, Quat q);
+Mat4 Mat4_inverse_rigid(Mat4 src);
+Mat4 Mat4_mul(Mat4 a, Mat4 b);
 
 /*** FUNCTION DEFINITIONS ***/
 
 #ifdef CIMPL_IMPLEMENTATION
-Mat3 Mat4_rotation(Mat4* m) {
+
+/* Vec3 */
+f32 Vec3_length(Vec3 v) { return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z); }
+
+Vec3 Vec3_normalize(Vec3 v) {
+    f32 mag = Vec3_length(v);
+    v.x = v.x / mag;
+    v.y = v.y / mag;
+    v.z = v.z / mag;
+    return v;
+}
+
+Vec3 Vec3_cross(Vec3 a, Vec3 b) {
+    Vec3 c = {0};
+    c.x = a.y * b.z - a.z * b.y;
+    c.y = a.z * b.x - a.x * b.z;
+    c.z = a.x * b.y - a.y * b.x;
+    return c;
+}
+
+Vec3 Vec3_translation_from_mat4(Mat4 m) { return (Vec3){m.ti, m.tj, m.tk}; }
+
+/* Quat */
+f32 Quat_length(Quat q) {
+    return sqrtf(q.x * q.x + q.y * q.y + q.z * q.z + q.w * q.w);
+}
+Quat Quat_normalize(Quat q) {
+    f32 mag = Quat_length(q);
+    q.x = q.x / mag;
+    q.y = q.y / mag;
+    q.z = q.z / mag;
+    q.w = q.w / mag;
+    return q;
+}
+
+/* Mat3 */
+Mat3 Mat3_from_quat(Quat q) {
+    // Ensure it is normalized
+    q = Quat_normalize(q);
+    float xx = q.x * q.x;
+    float yy = q.y * q.y;
+    float zz = q.z * q.z;
+    float xy = q.x * q.y;
+    float xz = q.x * q.z;
+    float yz = q.y * q.z;
+    float wx = q.w * q.x;
+    float wy = q.w * q.y;
+    float wz = q.w * q.z;
+
+    Mat3 dst;
+
+    dst.xi = 1.0f - 2.0f * (yy + zz);
+    dst.xj = 2.0f * (xy + wz);
+    dst.xk = 2.0f * (xz - wy);
+
+    dst.yi = 2.0f * (xy - wz);
+    dst.yj = 1.0f - 2.0f * (xx + zz);
+    dst.yk = 2.0f * (yz + wx);
+
+    dst.zi = 2.0f * (xz + wy);
+    dst.zj = 2.0f * (yz - wx);
+    dst.zk = 1.0f - 2.0f * (xx + yy);
+
+    return dst;
+}
+
+Mat3 Mat3_rotation_from_mat4(Mat4 m) {
     return (Mat3){
         // clang-format off
-        m->xi, m->xj, m->xk,
-        m->yi, m->yj, m->yk,
-        m->zi, m->zj, m->zk,
+        m.xi, m.xj, m.xk,
+        m.yi, m.yj, m.yk,
+        m.zi, m.zj, m.zk,
         // clang-format on
     };
 }
 
-Vec3 Mat4_translation(Mat4* m) {
-    return (Vec3){
+/* Mat4 */
+Mat4 Mat4_with_rotation(Mat4 dst, Mat3 src) {
+    dst.xi = src.xi;
+    dst.xj = src.xj;
+    dst.xk = src.xk;
+    dst.yi = src.yi;
+    dst.yj = src.yj;
+    dst.yk = src.yk;
+    dst.zi = src.zi;
+    dst.zj = src.zj;
+    dst.zk = src.zk;
+    return dst;
+}
+
+void Mat4_print_with_id(Mat4 m, const char* id) {
+    printf(
+        "%s: [\n"
+        "  %9.3f, %9.3f, %9.3f, %9.3f\n"
+        "  %9.3f, %9.3f, %9.3f, %9.3f\n"
+        "  %9.3f, %9.3f, %9.3f, %9.3f\n"
+        "  %9.3f, %9.3f, %9.3f, %9.3f\n"
+        "]\n",
+        id,
         // clang-format off
-        m->ti, m->tj, m->tk,
+        m.xi, m.yi, m.zi, m.ti,
+        m.xj, m.yj, m.zj, m.tj,
+        m.xk, m.yk, m.zk, m.tk,
+        m.xw, m.yw, m.zw, m.tw
         // clang-format on
-    };
+    );
+}
+
+Mat4 Mat4_from_translation_quat(Vec3 t, Quat q) {
+    Mat4 m = MAT4_IDENTITY;
+    m.ti = t.x;
+    m.tj = t.y;
+    m.tk = t.z;
+    Mat3 R = Mat3_from_quat(q);
+    return Mat4_with_rotation(m, R);
+}
+
+Mat4 Mat4_inverse_rigid(Mat4 src) {
+    Mat4 dst = MAT4_IDENTITY;
+    Vec3 x = Vec3_normalize((Vec3){src.xi, src.xj, src.xk});
+    Vec3 y = Vec3_normalize((Vec3){src.yi, src.yj, src.yk});
+    Vec3 z = Vec3_normalize((Vec3){src.zi, src.zj, src.zk});
+    dst.xi = x.x;
+    dst.xj = y.x;
+    dst.xk = z.x;
+
+    dst.yi = x.y;
+    dst.yj = y.y;
+    dst.yk = z.y;
+
+    dst.zi = x.z;
+    dst.zj = y.z;
+    dst.zk = z.z;
+
+    dst.ti = -(x.x * src.ti + x.y * src.tj + x.z * src.tk);
+    dst.tj = -(y.x * src.ti + y.y * src.tj + y.z * src.tk);
+    dst.tk = -(z.x * src.ti + z.y * src.tj + z.z * src.tk);
+
+    return dst;
+}
+
+Mat4 Mat4_mul(Mat4 a, Mat4 b) {
+    Mat4 dst = MAT4_IDENTITY;
+    dst.xi = a.xi * b.xi + a.yi * b.xj + a.zi * b.xk + a.ti * b.xw;
+    dst.xj = a.xj * b.xi + a.yj * b.xj + a.zj * b.xk + a.tj * b.xw;
+    dst.xk = a.xk * b.xi + a.yk * b.xj + a.zk * b.xk + a.tk * b.xw;
+    dst.xw = a.xw * b.xi + a.yw * b.xj + a.zw * b.xk + a.tw * b.xw;
+
+    dst.yi = a.xi * b.yi + a.yi * b.yj + a.zi * b.yk + a.ti * b.yw;
+    dst.yj = a.xj * b.yi + a.yj * b.yj + a.zj * b.yk + a.tj * b.yw;
+    dst.yk = a.xk * b.yi + a.yk * b.yj + a.zk * b.yk + a.tk * b.yw;
+    dst.yw = a.xw * b.yi + a.yw * b.yj + a.zw * b.yk + a.tw * b.yw;
+
+    dst.zi = a.xi * b.zi + a.yi * b.zj + a.zi * b.zk + a.ti * b.zw;
+    dst.zj = a.xj * b.zi + a.yj * b.zj + a.zj * b.zk + a.tj * b.zw;
+    dst.zk = a.xk * b.zi + a.yk * b.zj + a.zk * b.zk + a.tk * b.zw;
+    dst.zw = a.xw * b.zi + a.yw * b.zj + a.zw * b.zk + a.tw * b.zw;
+
+    dst.ti = a.xi * b.ti + a.yi * b.tj + a.zi * b.tk + a.ti * b.tw;
+    dst.tj = a.xj * b.ti + a.yj * b.tj + a.zj * b.tk + a.tj * b.tw;
+    dst.tk = a.xk * b.ti + a.yk * b.tj + a.zk * b.tk + a.tk * b.tw;
+    dst.tw = a.xw * b.ti + a.yw * b.tj + a.zw * b.tk + a.tw * b.tw;
+
+    return dst;
 }
 
 CimplReturn StlTriangleArray_from_binary(
